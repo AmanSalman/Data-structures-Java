ab0cb244196dad750e278778c63480cf36d59d62
AmanSalman <rapamansalman@gmail.com>
2024-09-19 22:55:47 +0300

    add Node.java

---
+++ Tree/Node.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aman
+ */
+public class Node {
+    Node right;
+    Node left;
+    Integer data;
+
+    public Node( Integer data) {
+        this.data = data;
+    }
+    
+}
